1811f69b4ad49def7348f52bb714591049f4dbd4
raamenzurururu <60289223+raamenzurururu@users.noreply.github.com>
2020-07-23 15:16:54 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -19,14 +19,14 @@ import "firebase/firebase";
 Vue.use(firestorePlugin);
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB-5fUrXhKNdTL-5s9pBn4LRGgFNmOFbhc",
-  authDomain: "practice-firebase-f1854.firebaseapp.com",
-  databaseURL: "https://practice-firebase-f1854.firebaseio.com",
-  projectId: "practice-firebase-f1854",
-  storageBucket: "practice-firebase-f1854.appspot.com",
-  messagingSenderId: "1020702246786",
-  appId: "1:1020702246786:web:3fc8e2f129cfc0d890b19b",
-  measurementId: "G-KPT6H4417J",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 export const db = firebase.firestore()
